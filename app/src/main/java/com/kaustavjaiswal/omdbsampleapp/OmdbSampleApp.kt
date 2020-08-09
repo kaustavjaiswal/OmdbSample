db49c2f3fcbb91caca1da339ed5da1846b61397d
Kaustav Jaiswal <kaustavj@gmail.com>
2020-08-09 13:26:54 +0530

    Set up:
    * themes.xml
    * Application Class
    * Manifest Permissions

---
+++ app/src/main/java/com/kaustavjaiswal/omdbsampleapp/OmdbSampleApp.kt
@@ -0,0 +1,17 @@
+package com.kaustavjaiswal.omdbsampleapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
+
+@HiltAndroidApp
+class OmdbSampleApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+}
